bada5b908632b034a6a35ca47af070911f988506
Juan Rodriguez <juan.rodriguez@helloiconic.com>
2022-01-07 03:12:23 -0600

    Add dto

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,20 @@
-export class CreatePostDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreatePostDto {
+
+    @IsNotEmpty()
+    id?: number
+
+    @IsNotEmpty()
+    @IsString()
+    title: string
+
+    @IsNotEmpty()
+    @IsString()
+    description: string
+
+    @IsNotEmpty()
+    @IsString()
+    content: string
+
+}
